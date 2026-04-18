411bb9bc9869e38f556b0abb6620414d52a2934b
easywangzheng <easywangzheng@gmail.com>
2026-04-18 21:56:49 +0300

    Update website content: fix HTTPS, update contact info, remove emergency contact, update form submission to info@jha-arabia.com

--- js/main.js
+++ js/main.js
@@ -53,9 +53,8 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             
             // In a real application, you would send this to a server
-            // For now, just show a success message
-            alert('Thank you for your message! We will get back to you soon.');
-            this.reset();
+            // For now, redirect to success page
+            window.location.href = 'form-handler.html';
         });
     }
     
